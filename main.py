571ab73e65ae44a8fbb1d50fa27e74f3ecf1ee78
vic-tor-ia0314 <vcao9784@gmail.com>
2026-02-28 15:56:18 -0500

    updated main.py

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pygame.display.set_caption("bait")
 
 clock = pygame.time.Clock()
 
+def title():
+    pass
+
+def game():
+    pass
+
+def endscreen():
+    pass
+
+
+#MAIN
+
+coins = 0
+
+status = "title"
+
 running = True
+
 while running:
     for event in pygame.event.get(): 
         if event.type == KEYDOWN:
