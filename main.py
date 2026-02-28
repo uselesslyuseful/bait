d98365cb226a67de0fe27fdb52bac3d74dd823c7
susan <susan.ruoxuan.zheng@gmail.com>
2026-02-28 15:26:41 -0500

    base loop

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get(): 
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+    
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
